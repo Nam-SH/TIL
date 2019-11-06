b8eac97c2d70defaec848392cda0bc2f1916a27f
Nam-SH <gtsmell@gmail.com>
2019-11-06 17:10:39 +0900

    20191106 | Vue

---
+++ 06_vue/02_vue_webpack/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>여기는 vue 최상위 문서</h1>
+  <todo-list category="취업특강"></todo-list>
+  <todo-list category="SSAFY"></todo-list>
+  <todo-list category="기타"></todo-list>
+  </div>
+</template>
+
+<script>
+  import TodoList from './components/TodoList.vue'
+
+  export default {
+    components: {
+      TodoList
+    }
+  }
+</script>
+
+<style>
+
+</style>
