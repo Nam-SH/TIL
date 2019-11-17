c5ff79c0a3d1f89f94e234c2e9512f8102c0445f
Nam-SH <gtsmell@gmail.com>
2019-11-17 21:37:41 +0900

    20191117 | Update

---
+++ 06_vue/00_vue_intro/testy.vue
@@ -0,0 +1,23 @@
+<template>
+ <div id="app-2">
+  <p v-text="message"></p>
+  <p>{{ message }}</p>
+</div>
+</template>
+
+
+<script>
+  export default {
+    name: 'app-2',
+    data () {
+      return {
+        message: '<h1> hi </h1>',
+      }
+    }
+  }
+</script>
+
+
+<style>
+
+</style>
