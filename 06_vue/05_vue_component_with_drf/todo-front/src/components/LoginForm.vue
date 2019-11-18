5ed291f9dfd0a7312d09bbc2a76bdd15b113a535
Nam-SH <gtsmell@gmail.com>
2019-11-18 15:30:04 +0900

    20191118 | Vue_JWT

---
+++ 06_vue/05_vue_component_with_drf/todo-front/src/components/LoginForm.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="login-div">
+
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+
+    <div v-else class="login-form">
+      <div v-if="errors.length" class="error-list alert alert-danger" role="alert">
+        <h4>다음의 오류를 해결 좀 해주세요;;</h4>
+        <hr>
+        <div v-for="(error, idx) in errors" :key="idx"> {{ error }} </div>
+      </div>
+
+      <div class="form-group">
+        <label for="id">ID</label>
+        <input 
+        type="text" 
+        class="form-control" 
+        id="id" 
+        placeholder="아이디 입력ㄱㄱ해주세요"
+        v-model="credentials.username"
+        @keyup.enter="login"
+        >
+      </div>
+      <div class="form-group">
+        <label for="password">PASSWORD</label>
+        <input 
+        type="password" 
+        class="form-control" 
+        id="password" 
+        placeholder="비밀번호 입력ㄱㄱ해주세요"
+        v-model="credentials.password"
+        @keyup.enter="login"
+        >
+      </div>
+      <div>
+        <button class="btn btn-primary" @click="login"> 로그인!! </button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+
+  import axios from 'axios'
+
+  export default {
+    name: 'LoginForm',
+    data () {
+      return {
+        credentials: {
+          username: '',
+          password: '',
+        },
+        loading: false,
+        errors: [],
+      }
+    },
+    methods: {
+      login () {
+        if (this.checkForm()) {
+          this.loading = true
+          axios.get('http://127.0.0.1:8000', this.credentials)
+          .then(res => {
+            console.log(res)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        } else {
+          console.log('로그인 검증 실패;;')
+        }
+      },
+      checkForm() {
+        this.errors = []
+        if (!this.credentials.username) {
+          this.errors.push("아이디 입력 필요함;;")
+        }
+        if (this.credentials.password.length < 8) {
+          this.errors.push("비밀번호는 8자 이상 입력해야됨;;")
+        }
+        if (this.errors.length === 0) {
+          return true
+        }
+      }
+    }
+    
+  }
+</script>
+
+
+<style>
+
+</style>
